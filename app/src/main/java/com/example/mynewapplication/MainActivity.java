c453766b1424a247f6b3ad1462516b5119926df9
Karrina_msw <moussaoui.karina@gmail.com>
2020-05-25 10:49:07 +0200

    [feature][API] PokeApi

--- app/src/main/java/com/example/mynewapplication/MainActivity.java
+++ app/src/main/java/com/example/mynewapplication/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.mynewapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String BASE_URL = "https://pokeapi.co/";
 
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
@@ -20,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        showList();
+        makeApiCall();
+    }
+
+    private void showList() {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         // use this setting to
         // improve performance if you know that changes
@@ -29,11 +48,60 @@ public class MainActivity extends AppCompatActivity {
         // use a linear layout manager
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+
         List<String> input = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             input.add("Test" + i);
-        }// define an adapter
+        }
+
+        // define an adapter
         mAdapter = new ListAdapter(input);
         recyclerView.setAdapter(mAdapter);
+    }
+
+
+    private void makeApiCall(){
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        PokeApi pokeApi = retrofit.create(PokeApi.class);
+
+        Log.d("VINCE","BEFORE CALLBACK");
+        Call<RestPokemonResponse> call = pokeApi.getPokemonResponse();
+        call.enqueue(new Callback<RestPokemonResponse>(){
+
+            @Override
+            public void onResponse( Call<RestPokemonResponse> call,  Response<RestPokemonResponse> response) {
+                Log.d("VINCE","INSIDE CALLBACK");
+
+
+                if(response.isSuccessful() && response.body() != null){
+                        List<Pokemon> pokemonList = response.body().getResults();
+                        Toast.makeText( getApplicationContext(),"API Success", Toast.LENGTH_SHORT).show();
+                } else{
+                    showError();
+                }
+
+            }
+
+            @Override
+
+            public void onFailure(@NonNull Call<RestPokemonResponse> call, Throwable t) {
+                showError();
+
+            }
+        });
+        Log.d("VINCE","AFTER CALLBACK");
+
+    }
+    private void showError(){
+        Toast.makeText(getApplicationContext(), "API Error", Toast.LENGTH_SHORT).show();
+
     }
 }
---
+++ app/src/main/java/com/example/mynewapplication/Pokemon.java
@@ -0,0 +1,14 @@
+package com.example.mynewapplication;
+
+public class Pokemon {
+    private String name;
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
